e484b78ae9dbc37ade618494d011a3a757f0428c
lenandlar <lenandlar@gmail.com>
2016-12-01 23:27:20 -0400

    Update TaxCalc.js

--- TaxCalc.js
+++ TaxCalc.js
@@ -3,32 +3,57 @@ function doMTax(){
 
 	var gross = parseInt(document.tCalc.val1.value); // collect and convert income data to number
 	
-	var tax = 0;
+	var emptax = 0;
+	var selfemptax = 0;
 	var tr1 = 0.28;
 	var tr2 = 0.40;
 	var tho1 = 60000;
 	var tho1a = 33600;
-	var tho2 = gross * 0.67;
+	var tho2 = gross * 0.6667;
+	var empNIS = 0;
+	var selfempNIS = 0;
 	
 	if ((isNaN(gross)) || (gross < 0)){
 		alert("incorrect value entered for Gross Income");
 		document.tCalc.f1.value = "";
 		document.getElementById("f1").focus();
 	}
+
 	else
 	if (gross <= 60000){
-	  tax = 0;
+	  empNIS = gross * 0.056;
+	  selfempNIS = gross * 0.125;
+	  emptax = 0;
+	  selfemptax = 0;
 	}
+
 	else
 	if (gross <= 180000){
-	 tax = (gross - tho1) * 0.28;
+         empNIS = gross * 0.056;
+	 selfempNIS = gross * 0.125;
+	 emptax = (gross - tho1 - empNIS) * 0.28;
+	 selfemptax = (gross - tho1 - selfempNIS) * 0.28;
+	}
+
+	else
+	if (gross <= 200000){
+	 empNIS = gross * 0.056;
+	 selfempNIS = gross * 0.125;
+	 emptax = ((tho2 - 120000 - empNIS)*0.4) + tho1a;
+	 selfemptax = ((tho2 - 120000 - selfempNIS)*0.4) + tho1a;
 	}
 
 	else {
-	 tax = ((tho2 - 120000)*0.4) + tho1a;
+	 empNIS = 200000 * 0.056;
+	 selfempNIS = 200000 * 0.125;
+	 emptax = ((tho2 - 120000 - empNIS)*0.4) + tho1a;
+	 selfemptax = ((tho2 - 120000 - selfempNIS)*0.4) + tho1a;
 	}
 
-	document.tCalc.tax1.value = tax; // display tax data to form element
+	document.tCalc.tax1.value = emptax; // display emp tax 
+	document.tCalc.tax1_1.value = selfemptax; //display self emp tax
+	document.tCalc.nis1.value = empNIS; // display emp NIS 
+	document.tCalc.nis1_1.value = selfempNIS; // display self emp 
 
 }
 
@@ -36,32 +61,57 @@ function doYTax(){
 
 	var gross = parseInt(document.tCalc1.val1.value); // collect and convert income data to number
 	
-	var tax = 0;
+	var emptax = 0;
+	var selfemptax =0;
 	var tr1 = 0.28;
 	var tr2 = 0.40;
 	var tho1 = 720000;
 	var tho1a = 33600;
 	var tho2 = gross * 0.6667;
-	
+	var empNIS = 0;
+	var selfempNIS = 0;
+
 	if ((isNaN(gross)) || (gross < 0)){
 		alert("incorrect value entered for Gross Income");
 		document.tCalc1.f1_1.value = "";
 		document.getElementById("f1_1").focus();
 	}
+
 	else
 	if (gross <= 720000){
-	  tax = 0;
+          empNIS = gross * 0.056;
+	  selfempNIS = gross * 0.125;
+	  emptax = 0;
+          selfemptax = 0;
 	}
+
 	else
 	if (gross <= 2160000){
-	 tax = (gross - tho1) * 0.28;
+  	  empNIS = gross * 0.056;
+	  selfempNIS = gross * 0.125;
+	  emptax = (gross - tho1 - empNIS) * 0.28;
+	  selfemptax = (gross - tho1 - selfempNIS) * 0.28;
+	}
+
+	else
+	if (gross <= 2400000){
+	 empNIS = gross * 0.056;
+	 selfempNIS = gross * 0.125;
+	 emptax = ((tho2 - (120000 * 12) - empNIS)*0.4) + (tho1a * 12);
+	 selfemptax = ((tho2 - (120000 * 12) - selfempNIS)*0.4) + (tho1a * 12);
 	}
 
 	else {
-	 tax = ((tho2 - 1440000)*0.4) + (tho1a * 12);
+         empNIS = 2400000 * 0.056;
+	 selfempNIS = 2400000 * 0.125;
+         emptax = ((tho2 - (120000 * 12) - empNIS)*0.4) + (tho1a * 12);
+	 selfemptax = ((tho2 - (120000 * 12) - selfempNIS)*0.4) + (tho1a * 12);
 	}
 
-	document.tCalc1.tax1.value = tax; // display tax data to form element
+	document.tCalc1.tax1.value = emptax; // display emp tax
+	document.tCalc1.tax1_1.value = selfemptax; //display self emp tax
+	document.tCalc1.nis2.value = empNIS; // display emp NIS 
+	document.tCalc1.nis2_1.value = selfempNIS; // display self emp 
 
 }
 
